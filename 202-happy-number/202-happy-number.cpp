f3a052912ed4826ca4e3dc0776085f60114428d2
Abd2lhakam19 <105023622+Abd2lhakam19@users.noreply.github.com>
2022-05-12 18:55:29 +0200

    Time: 3 ms (57.06%), Space: 5.7 MB (94.96%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int ans = 0;//19
+ while(n>9)
+ {
+      ans = 0;
+     while(n>0)
+     {
+
+         int rem = pow(n%10,2);
+         ans+=rem;
+         n/=10;
+     }
+     n = ans;
+ }
+ if(n==1||n==7)
+ {
+     return true;
+ }
+ else
+    return false;
+    }
+};
